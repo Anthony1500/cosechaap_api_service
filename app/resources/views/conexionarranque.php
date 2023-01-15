d32885a2d3c68634e12ca2e01e3c9f1cf7c549d6
Anthony <arteaga17000@gmail.com>
2023-01-15 17:12:41 -0500

    0.7

--- app/resources/views/conexionarranque.php
+++
@@ -1,9 +0,0 @@
-<?php
-class conexion {
-    public $hostname = "localhost";
-    public $database = "coseuuhl_k7jTW1fAmX";
-    public $username = "coseuuhl_k7jTW1fAmX";
-    public $password = "nZwp5aoAz8";
-    public $conn;
-}
-?>
